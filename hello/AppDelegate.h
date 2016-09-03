bf10b0ef350a61120e21e246f023c64bb756aa05
李旦 <eric_l@sina.com>
2016-09-04 04:30:42 +0800

    Initial Commit

---
+++ hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by lid on 16/9/4.
+//  Copyright © 2016年 lid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by lid on 16/9/4.
+//  Copyright © 2016年 lid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
